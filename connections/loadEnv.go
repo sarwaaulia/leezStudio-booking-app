fed422884f1bb452331807a2e2cac361e535d965
sarwa aulia <sarwaaulia01@example.com>
2026-03-06 23:19:20 +0700

    fix env

--- connections/loadEnv.go
+++ connections/loadEnv.go
@@ -1,14 +1,15 @@
 package connections
 
 import (
- "log"
+ "fmt"
 
  "github.com/joho/godotenv"
 )
 
+// Di connections/loadEnv.go
 func LoadEnvVariables() {
-	err:=godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading env")
-	}
+    err := godotenv.Load()
+    if err != nil {
+        fmt.Println("Running in production mode (No .env file found)")
+    }
 }
